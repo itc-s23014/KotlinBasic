60100a10c4e8c8563a84919637326f412073415f
itc-s23014 <s23014@std.it-college.ac.jp>
2024-05-08 12:02:10 +0900

    ２章終わり

---
+++ src/main/kotlin/Chap02.kt
@@ -0,0 +1,10 @@
+package org.example
+
+fun main(args: Array<String>) {
+    val message : String = "Hello World"
+    println("Hello, World!")
+    println("こんにちは")
+    println("はじめまして")
+    println("よろしく！")
+    println(message)
+}
