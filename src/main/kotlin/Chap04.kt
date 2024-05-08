14bb81f3d71ab15d00a9e8b393ca813f7ba6e428
itc-s23014 <s23014@std.it-college.ac.jp>
2024-05-08 14:40:25 +0900

    4章終わり

--- src/main/kotlin/Chap04.kt
+++ src/main/kotlin/Chap04.kt
@@ -10,5 +10,5 @@ fun main(args: Array<String>) {
     val g: UByte = 150U
     val h: UShort = 33000U
     val i: UInt = 2200000U
-    val j: ULong = 10000000000000000UL
+    val j: ULong = 1000000000000000UL
 }
