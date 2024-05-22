31adf67458be2541e31f161ae055331ac92c308d
itc-s23014 <s23014@std.it-college.ac.jp>
2024-05-22 11:20:24 +0900

    31章終わり

---
+++ src/main/kotlin/Chap31.kt
@@ -0,0 +1,34 @@
+package com.example.chap31
+
+fun main(args: Array<String>) {
+        val persoon = FlyingPerson("タケシ",5)
+        val car = FlyingCar("赤")
+        makeItFly(persoon)
+        makeItFly(car)
+}
+fun makeItFly(a: Flyable){
+    a.fly()
+}
+
+interface Flyable{
+    fun fly(){
+        println("未確認飛行物体")
+    }
+
+}
+open class Person(val name: String, val age: Int)
+
+class FlyingPerson(name: String,age: Int) : Person(name, age),Flyable {
+    override fun fly() {
+        println("まさか！人間が空を飛んでいます。")
+    }
+
+}
+abstract class Car(val color: String)
+
+class FlyingCar(color: String) : Car(color),Flyable {
+    override fun fly() {
+        println("すごい！車が空を飛んでいます。")
+    }
+
+}
